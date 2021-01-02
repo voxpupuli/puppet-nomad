32e24f4d3a936ca11504f6052db4b4962fee8998
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-01-02 15:55:52 +0100

    Use spec_helper_acceptance from modulesync

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,23 +1,7 @@
-require 'beaker-rspec/spec_helper'
-require 'beaker-rspec/helpers/serverspec'
-require 'beaker/puppet_install_helper'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-#run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-
-RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'nomad')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppet-stdlib'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppet-archive'), { :acceptable_exit_codes => [0,1] }
-    end
-  end
+configure_beaker do |host|
+  host.install_package('unzip')
 end
+
+Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
