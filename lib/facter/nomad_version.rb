3ce4c027743a3d788d2db835a7f9c23f99ea6bef
Tim Meusel <tim@bastelfreak.de>
2021-05-14 12:00:41 +0200

    rubocop: autofix

--- lib/facter/nomad_version.rb
+++ lib/facter/nomad_version.rb
@@ -1,10 +1,10 @@
 # nomad_version.rb
 #
 Facter.add(:nomad_version) do
-  confine :kernel => 'Linux'
+  confine kernel: 'Linux'
   setcode do
     begin
-      Facter::Util::Resolution.exec('nomad --version 2> /dev/null').lines.first.split[1].tr('v','')
+      Facter::Util::Resolution.exec('nomad --version 2> /dev/null').lines.first.split[1].tr('v', '')
     rescue
       nil
     end
